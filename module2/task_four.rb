07e7592ac4a590430d63703f04953b2ff7e10ba6
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-08-11 10:16:50 -0400

    used each.with_index method

--- module2/task_four.rb
+++ module2/task_four.rb
@@ -1,9 +1,10 @@
 alphabet = ('a'..'z').to_a
 
+arr_vowels = %w( a e i o u y)
 hash = {}
 
-alphabet.each_index do |i|
-  if ['a','e','i','o','u','y'].include?(alphabet[i])
+alphabet.each.with_index(0) do |value,i|
+  if arr_vowels.include?(alphabet[i])
     key = alphabet[i]
     hash[key] = i+1
   end
