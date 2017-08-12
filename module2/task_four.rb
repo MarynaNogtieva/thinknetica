e1d262c1a2fa87a6f4c038b2cb894aa3981462fc
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-08-12 10:35:33 -0400

    used index 1

--- module2/task_four.rb
+++ module2/task_four.rb
@@ -1,11 +1,11 @@
 alphabet = ('a'..'z').to_a
 
-arr_vowels = %w( a e i o u y)
+arr_vowels = %w(  a e i o u y)
 hash = {}
 
-alphabet.each.with_index(0) do |value,i|
+alphabet.each.with_index(1) do |value,i|
   if arr_vowels.include?(value)
-    hash[value] = i+1
+    hash[value] = i
   end
 end
 
