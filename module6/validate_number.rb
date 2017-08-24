2554696d5b53cef0a7b398b0c45d9fbc1d9d4566
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-08-23 21:05:49 -0400

    Add validate number module

--- module6/validate_number.rb
+++ module6/validate_number.rb
@@ -1,2 +1,10 @@
 module ValidateNumber
+  VALID_NUMBER_FORMAT = /^[a-z\d]{3}-?[a-z\d]{2}$/i
+  def validate_number! (number)
+    raise "Number cannot be nil" if number.nil?
+    raise "Number must be at least 6 characters" if number.to_s.length < 6
+    raise "Number must have 3 letters and/or numbers,
+           hypen(optional) and 2 letters and/or numbers" if number !~ VALID_NUMBER_FORMAT
+    true
+  end
 end
