e5683f0792dc9cbe6c710e667f247508cd6952cc
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-09-03 17:21:37 -0400

    fix accessors module

--- module9/module/accessors.rb
+++ module9/module/accessors.rb
@@ -6,32 +6,39 @@ module Acessors
   end
 
   module ClassMethods
-    def  attr_accessor_with_history(*methods)
+    def strong_attr_acessor(*attr_names, klass)
+      attr_names.each do |attr_name|
+        define_method("#{attr_name}") do
+          instance_variable_get("@#{attr_name}")
+        end
+
+        # check type of the attribute before using setter
+        define_method("#{attr_name}=") do |val|
+          raise TypeError.new("Wrong type of instance was passed") unless val.is_a?(klass)
+          instance_variable_set("@#{attr_name}", val)
+        end
+      end
+    end
+
+    def attr_accessor_with_history(*methods)
       methods.each do |method|
-        #getter for method
         method_name = method.to_sym
         define_method("#{method_name}") do
           instance_variable_get("@#{method_name}")
         end
 
-       #setter for method
         define_method("#{method_name}=") do |value|
-          #deal with history of this method
           history = instance_variable_get("@#{method_name}_history")
           history ||= []
           history << value
-          #setter for history of the method
-          instance_variable_set("@#{method_name}_history",history)
-          #setter for method
+          instance_variable_set("@#{method_name}_history", history)
           instance_variable_set("@#{method_name}", value)
         end
 
-        #getter for the history of a given method
         define_method("#{method_name}_history") do
           instance_variable_get("@#{method_name}_history") || []
         end
       end
     end
   end
-end
 end
