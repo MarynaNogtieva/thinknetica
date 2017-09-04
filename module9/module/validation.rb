1490f81962574364d52ba3e3f6bf5379fa757d5f
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-09-04 16:03:21 -0400

    update validation

--- module9/module/validation.rb
+++ module9/module/validation.rb
@@ -8,17 +8,22 @@ module Validation
 
   module ClassMethods
     attr_accessor :validation_hash
+    # validation_hash = {name: [{presense: name}, {format: 'w'}, {type: string}]}
 
     def validate(attr_name, validation_type, options = {})
-     validation_array =  validation_array || [];
-     validation_array << {type: validation_type, options: options}
+     validation_array ||= [];
+     hash =
+     validation_array << { validation_type: options}
      @validation_hash[attr_name.to_sym] = validation_array
     end
   end
 
   module InstanceMethods
     def validate!
-
+      self.class.validation_hash.each do |attr_name, array|
+      instance = get_instance_by_attr_name(attr_name)
+      array.each {|hash| send(hash[:type],instance, hash[:options])}
+      end
     end
 
 
@@ -32,16 +37,19 @@ module Validation
     private
 
     def presence(attr_name)
-      raise 'Attribute cannot be nill or empty' if attr_empty? && attr_name.is_a?(String)
+      raise 'Attribute cannot be nill or empty' if attr_name.empty? && attr_name.is_a?(String)
       raise 'Array cannot be empty' if attr_name.count.zero? && attr_name.is_a?(Array)
     end
 
-    def format(attr_name, regex)
-      raise 'Wrong format was passed' if attr_name !~ regex
+    def format(attr_name, options)
+      oprions.each do |key, value_format|
+      raise 'Wrong format was passed' if attr_name !~ value_format
     end
 
-    def type(attr_name, class_name)
-      raise "This attribute does not correspont to #{class_name}.to_s" unless attr_name.is_a?(class_name)
+    def type(attr_name, options)
+      options.each_value do |class_name|
+        raise "This attribute does not correspont to #{class_name}.to_s" unless attr_name.is_a?(class_name)
+      end
     end
 
     def attr_empty?(attr_name)
@@ -49,7 +57,7 @@ module Validation
     end
 
     def get_instance_by_attr_name(name)
-      instance_variable_get("@{name}".to_s)
+      instance_variable_get("@#{name}")
     end
   end
 end
