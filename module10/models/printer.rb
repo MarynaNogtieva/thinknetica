480faf5b56a30c6d25cf93a81911021dd3a8625a
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-09-11 21:22:21 -0400

    add printing methods

--- module10/models/printer.rb
+++ module10/models/printer.rb
@@ -1,15 +1,24 @@
-
+require './models/player'
 
 class Printer 
   
+  #this method will be moved to some other class
   def create_player
+    name = player_name
+    type = player_type
+    
+    player = Player.new(name: name, type: type)
+    player
+  end
+    
+  def player_name
     print "Enter player name: "
     name = gets.chomp
-    type = player_type
+    name
   end
-
+  
   def player_type
-    print "Enter player type: dealer or player"
+    print 'Enter player type: dealer or player'
     type = gets.chomp.downcase
     
     while  not_valid_player_type?(type) do
@@ -19,6 +28,37 @@ class Printer
     
     type.to_sym
   end
+  
+  def show_base_actions
+    puts 'Enter n - to start a new game'
+    puts 'Enter q - to quit game'
+  end
+
+  def show_player_choice
+    puts "Enter 'hit' if you want to continue"
+    puts "Enter 'stay' if you want to skip"
+    puts "Enter 'show' if you want to open the cards"
+  end
+  
+  def show_dealer_choice
+    puts "Enter 'hit' if you want to continue"
+    puts "Enter 'stay' if you want to skip"
+  end
+  
+  def show_score(player, score)
+    puts "#{player.name}: #{score}"
+  end
+  
+  def show_bank(bank_money)
+    puts "#{bank_money}"
+  end
+  
+  def game_greeting
+    puts 'Welcome to our game! We will start now!'
+  end
+  
+  
+  private 
   
   def valid_player_type?(type)
     type == 'dealer' || type == 'player'
