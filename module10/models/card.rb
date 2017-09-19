2f35c0172476e3edbf341a2cbeaf1fdc6cfef19f
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-09-19 15:38:07 -0400

    fixed value assignment

--- module10/models/card.rb
+++ module10/models/card.rb
@@ -12,13 +12,7 @@ class Card
   end
   
   def assign_card_points(value)
-    cost = 0
-    if(value == 'J' || value == 'Q' || value == 'K' || value == 'A')
-      hash_points = {J: 10, Q: 10, K: 10, A: 11}
-      cost = hash_points[value.to_sym]
-    else
-      cost = value.to_i
-    end
+    cost = { J: 10, Q: 10, K: 10, A: 11 }.fetch(value.to_s.to_sym, value.to_i)
     cost  
   end
 end
